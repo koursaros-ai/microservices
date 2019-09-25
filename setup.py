f4a21c99dcffa9c87ea0067cd3d1d517c9e04000
colethienes <cole.thienes@gmail.com>
2019-09-25 14:26:47 -0400

    .

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='koursaros',
+    packages=['koursaros'],
+    version='0.0.1',
+    license='MIT',
+    description='Koursaros is a distributed, cloud-'
+                'native platform for developing and deploying '
+                'automated information retrieval and inference applications.',
+    author='Koursaros',
+    author_email='cole.thienes@gmail.com',
+    url='https://github.com/koursaros-ai/koursaros',
+    download_url='https://github.com/user/koursaros/archive/v_01.tar.gz',
+    keywords=['koursaros', 'distributed', 'cloud-native', 'neural', 'inference'],
+    install_requires=[  # I get to this in a second
+        'pika',
+    ],
+    classifiers=[
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
