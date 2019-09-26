de63ebcbef0f5bb5440ae6f090b4ca0a10cc760f
colethienes <cole.thienes@gmail.com>
2019-09-26 01:08:17 -0400

    .

--- kctl/deploy/yamls.py
+++ kctl/deploy/yamls.py
@@ -7,6 +7,11 @@ class Yamls:
         self.__dict__.update(entries)
 
 
+def yaml_safe_load(root, file):
+    with open(root + '/' + file) as fh:
+        return yaml.safe_load(fh)
+
+
 def compile_yamls(app_path):
     import os
 
@@ -15,15 +20,15 @@ def compile_yamls(app_path):
     for root, dirs, files in os.walk(app_path, topdown=False):
         for file in files:
             if file == 'connections.yaml':
-                yam = yaml.safe_load(root + '/' + file)
+                yam = yaml_safe_load(root, file)
                 yamls.add(**yam)
 
             elif file == 'service.yaml':
-                yam = yaml.safe_load(root + '/' + file)
+                yam = yaml_safe_load(root, file)
                 yamls.add(**yam)
 
             elif file == 'stubs.yaml':
-                yam = yaml.safe_load(root + '/' + file)
+                yam = yaml_safe_load(root, file)
                 print(yam)
                 yamls.add(**yam)
 
