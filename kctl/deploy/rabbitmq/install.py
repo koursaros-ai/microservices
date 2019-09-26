dd7b7fab206b21f77ee8c4ff4bf1b0d540127369
colethienes <cole.thienes@gmail.com>
2019-09-26 13:56:01 -0400

    .

--- kctl/deploy/rabbitmq/install.py
+++ kctl/deploy/rabbitmq/install.py
@@ -2,8 +2,8 @@ from sys import platform
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-result = sock.connect_ex(('127.0.0.1',80))
-if result == 5672:
+result = sock.connect_ex(('127.0.0.1',5672))
+if result == 0:
    print("Port is open")
 else:
    print("Port is not open")
