25e1e647d4261dde2a858aaf78cdb8505a9fda05
colethienes <cole.thienes@gmail.com>
2019-09-26 00:50:31 -0400

    .

--- kctl/deploy/yamls.py
+++ kctl/deploy/yamls.py
@@ -5,4 +5,4 @@ def compile_yamls(app_path):
     for root, dirs, files in os.walk(app_path, topdown=False):
         for file in files:
             if file.endswith('.yaml'):
-                print(file)
+                print(root + file)
