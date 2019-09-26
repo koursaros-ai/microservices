9e5d95ec4eeb5903f50325271599311216714611
colethienes <cole.thienes@gmail.com>
2019-09-26 02:04:25 -0400

    .

--- kctl/deploy/yamls.py
+++ kctl/deploy/yamls.py
@@ -17,23 +17,24 @@ def compile_yamls(app_path):
     yam = yaml_safe_load(app_path, 'connections.yaml')
     yamls.update(**yam)
 
-    pipelines = app_path + '/pipelines/*/'
-    for pipeline in glob(pipelines):
+    pipelines = app_path + '/pipelines/'
+
+    for pipeline in next(os.walk(pipelines))[1]:
         if not pipeline.startswith(INVALID_PIPELINE_PREFIXES):
             if 'pipelines' not in yamls:
                 yamls['pipelines'] = dict()
 
             print(pipeline)
-            stubs = yaml_safe_load(pipeline, 'stubs.yaml')
+            stubs = yaml_safe_load(pipelines + pipeline, 'stubs.yaml')
             yamls['pipelines'][pipeline] = stubs['stubs']
 
-    services = app_path + '/services/*/'
-    for service in glob(services):
+    services = app_path + '/services/'
+    for service in next(os.walk(services))[1]:
         if not service.startswith(INVALID_PIPELINE_PREFIXES):
             if 'services' not in yamls:
                 yamls['services'] = dict()
 
-            service = yaml_safe_load(service, 'services.yaml')
+            service = yaml_safe_load(services + service, 'services.yaml')
             yamls['services'][service] = service['service']
 
     import json
