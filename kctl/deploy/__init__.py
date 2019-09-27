3d0a5d631e40bdb4a9b6b2e50d2f75af83e18772
colethienes <cole.thienes@gmail.com>
2019-09-27 17:40:21 -0400

    .

--- kctl/deploy/__init__.py
+++ kctl/deploy/__init__.py
@@ -1,21 +1,31 @@
 import sys
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, STDOUT
 import os
 import signal
+from threading import Thread
+
+
+def log_subprocess_output(pipe):
+    for line in iter(pipe.readline, b''): # b'\n'-separated lines
+        print(line)
 
 
 def deploy_pipelines(app_path, services):
     app_name = app_path.split('/')[-2]
     os.chdir(app_path + '..')
     popens = []
+    threads = []
     try:
         for service in services:
             cmd = [sys.executable, '-m', f'{app_name}.services.{service}']
             print(f'Running {cmd}...')
-            sys.stdout.write('AAAAAAAA')
-            stdout = sys.stdout
-            stderr = sys.stderr
-            popen = Popen(cmd, stdout=stdout,  stderr=stderr)
+            popen = Popen(cmd, stdout=PIPE,  stderr=STDOUT)
+
+            with popen.stdout:
+                t = Thread(target=log_subprocess_output, args=(popen.stdout,))
+                t.start()
+                threads.append(t)
+
             popens.append((popen, service))
 
         for popen, service in popens:
