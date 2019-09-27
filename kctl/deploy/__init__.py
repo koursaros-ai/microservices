5d6813ee0683eedc1bdda00ee4b383e037eb7b80
colethienes <cole.thienes@gmail.com>
2019-09-27 14:54:46 -0400

    .

--- kctl/deploy/__init__.py
+++ kctl/deploy/__init__.py
@@ -1,14 +1,16 @@
 import sys
 
 
-def run_service(app_path, service, imports):
+def run_service(app_path, service, _imports):
 
     sys.path.insert(0, app_path)
 
     module = __import__('services', fromlist=[service])
 
-    for _import in imports:
-        setattr(module, _import, __import__(_import))
+    for _import in _imports:
+        name = _import.__name__
+        print(f'Importing {name} to {service}...')
+        setattr(module, name, _import)
 
     main = getattr(getattr(module, service), 'main', None)
     if main:
@@ -19,11 +21,15 @@ def deploy_pipelines(app_path, services, imports):
     from multiprocessing import Process
     from threading import Thread
 
+    _imports = []
+    for import_ in imports:
+        _imports.append(__import__(import_))
+
     processes = []
     for service in services:
         p = Thread(
             target=run_service,
-            args=(app_path, service, imports)
+            args=(app_path, service, _imports)
         )
         p.start()
         print(f'Started process {p.getName()}: {service}...')
