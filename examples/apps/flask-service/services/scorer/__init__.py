91f6e114ac23aa8f11240e21b347990d5f4fefb5
Jack Pertschuk <jp954@cornell.edu>
2019-09-27 16:45:32 -0400

    .

--- examples/apps/flask-service/services/scorer/__init__.py
+++ examples/apps/flask-service/services/scorer/__init__.py
@@ -43,19 +43,19 @@ service = Service(__file__)
 @service.stub
 def rerank(claim_with_lines, publish):
     print('ranking')
-    # def score(lines):
-    #     claims = [claim_with_lines.claim.text] * len(lines)
-    #     return regression_model.classify(claims, lines)
-    #
-    # results = []
-    # for scores, inputs in batch_fn(BATCH_SIZE, score, claim_with_lines.lines):
-    #     for score, line in zip(scores, inputs):
-    #         results.append((score, line))
-    # results.sort(key=lambda x: x[0], reverse=True)
+    def score(lines):
+        claims = [claim_with_lines.claim.text] * len(lines)
+        return regression_model.classify(claims, lines)
+
+    results = []
+    for scores, inputs in batch_fn(BATCH_SIZE, score, claim_with_lines.lines):
+        for score, line in zip(scores, inputs):
+            results.append((score, line))
+    results.sort(key=lambda x: x[0], reverse=True)
     print("publishing")
     publish(service.messages.ClaimWithLines(
         claim=claim_with_lines.claim,
-        lines=["test", "test"] #[el[1] for el in results[:5]]
+        lines=[el[1] for el in results[:5]]
     ))
 
 
