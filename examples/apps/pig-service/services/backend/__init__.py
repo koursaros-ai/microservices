4497ec27585cad8fa7f7ab96533a1820590777e2
Jack Pertschuk <jp954@cornell.edu>
2019-09-27 12:40:11 -0400

    .

---
+++ examples/apps/pig-service/services/backend/__init__.py
@@ -0,0 +1,53 @@
+from koursaros import Service
+from flask import Flask, request, jsonify
+from queue import Queue
+import threading
+import uuid
+
+service = Service(__file__)
+app = Flask(__name__)
+sentences = dict()
+
+
+@app.route('/')
+def receive():
+    text = request.args.get('q')
+    if not text:
+        return jsonify({
+            "status": "failure",
+            "msg": "Please provide a sentence"
+        })
+    global sentences
+
+    sentence_id = str(uuid.uuid4())
+    queue = Queue()
+    sentences[sentence_id] = queue
+
+    sentence = service.messages.Sentence(id=sentence_id, text=text)
+    send_sentence(sentence)
+    return jsonify({
+        "status": "success",
+        "msg": queue.get()
+        })
+
+
+@service.stub
+def send_sentence(sentence, publish):
+    publish(sentence)
+
+
+@service.stub
+def receive(piggified, publish):
+    global sentences
+    sentences[piggified.sentence.id].put(piggified.pig_latin)
+
+
+def main():
+    threads = service.run()
+    threads.append(threading.Thread(target=app.run))
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
