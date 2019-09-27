7ee33d6b9a0d0cd1c9b3acc37ef6f7447e4663f9
Jack Pertschuk <jp954@cornell.edu>
2019-09-27 08:56:41 -0400

    .

--- examples/apps/flask-service/services/scorer/__init__.py
+++ examples/apps/flask-service/services/scorer/__init__.py
@@ -4,23 +4,23 @@ from utils.buffer import download_and_unzip
 from koursaros.utils.model import Roberta
 import os
 
-CHECKPOINT_FILE = 'checkpoint_best.pt'
-NAME = 'scorer'
-MODELS_DIR = f'./'  # where to score the model locally
-
-MODEL = f'{NAME}-model.tar.gz'  # bucket storage
-BATCH_SIZE = 8
-BUCKET = 'poloma-models'
-model_dir = MODELS_DIR + f'{NAME}-output/'
-if not os.path.isfile(model_dir + CHECKPOINT_FILE):
-    print('downloading model...')
-    download_and_unzip(BUCKET, MODEL, MODELS_DIR, archive=True)
-
-regression_model = Roberta(
-    MODELS_DIR + f'{NAME}-output/',
-    CHECKPOINT_FILE,
-    force_gpu=False
-)
+# CHECKPOINT_FILE = 'checkpoint_best.pt'
+# NAME = 'scorer'
+# MODELS_DIR = f'./'  # where to score the model locally
+#
+# MODEL = f'{NAME}-model.tar.gz'  # bucket storage
+# BATCH_SIZE = 8
+# BUCKET = 'poloma-models'
+# model_dir = MODELS_DIR + f'{NAME}-output/'
+# if not os.path.isfile(model_dir + CHECKPOINT_FILE):
+#     print('downloading model...')
+#     download_and_unzip(BUCKET, MODEL, MODELS_DIR, archive=True)
+#
+# regression_model = Roberta(
+#     MODELS_DIR + f'{NAME}-output/',
+#     CHECKPOINT_FILE,
+#     force_gpu=False
+# )
 
 service = Service(__file__)
 
