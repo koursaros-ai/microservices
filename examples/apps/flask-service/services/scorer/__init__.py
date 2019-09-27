fa297951aec34f86373198bdda48bad2d481df74
Jack Pertschuk <jp954@cornell.edu>
2019-09-27 14:03:06 -0400

    .

--- examples/apps/flask-service/services/scorer/__init__.py
+++ examples/apps/flask-service/services/scorer/__init__.py
@@ -65,7 +65,7 @@ def main():
     )
 
     threads = service.run()
-    threads.append(load_model_thread)
+    # threads.append(load_model_thread)
 
     for t in threads:
         print('starting thread scorer')
