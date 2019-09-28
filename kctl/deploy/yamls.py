4e1c7d330cba4f2ea356a7f0785a9e40f0b60cc2
colethienes <cole.thienes@gmail.com>
2019-09-28 13:38:05 -0400

    .

--- kctl/deploy/yamls.py
+++ kctl/deploy/yamls.py
@@ -190,5 +190,12 @@ def compile_app(app_path):
     with open(app_path + '/.koursaros/app.pickle', 'wb') as fh:
         pickle.dump(app, fh, protocol=pickle.HIGHEST_PROTOCOL)
 
+    import pdb; pdb.set_trace()
+
+    with open(app_path + '/.koursaros/app.pickle', 'rb') as fh:
+        app = pickle.load(fh)
+
+    import pdb; pdb.set_trace()
+
     return app
 
