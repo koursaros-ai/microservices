e2510747d4d6e99d53f4115ff32b46b9c2b4c09d
colethienes <cole.thienes@gmail.com>
2019-09-28 05:57:01 -0400

    .

--- kctl/deploy/yamls.py
+++ kctl/deploy/yamls.py
@@ -45,7 +45,7 @@ class App:
         stubs = []
         for pipeline in pipelines:
             print('ajisjfojaosjfo')
-            print(dir(self.pipelines[pipeline]))
+            print(dir(self.pipelines[pipeline].stubs))
             for stub in self.pipelines[pipeline].stubs.values():
 
                 if service == stub.service:
