cfec1089df6ee954fb3c19757e5b768c03c36937
colethienes <cole.thienes@gmail.com>
2019-10-01 02:09:39 -0400

    .

--- kctl/cli.py
+++ kctl/cli.py
@@ -18,6 +18,9 @@ koursaros.compile.set_imports(SAVE_PATH)
 
 CWD = os.getcwd()
 PIPE_PATH = find_pipe_path(CWD)
+PIPE_NAME = PIPE_PATH.split('/')[-1]
+print(PIPE_NAME)
+raise SystemExit
 KCTL_PATH = kctl.__path__[0]
 PIPE_TEMPLATE_PATH = KCTL_PATH + '/create/template/pipeline'
 SERVICE_TEMPLATE_PATH = KCTL_PATH + '/create/template/pipeline/services/service'
