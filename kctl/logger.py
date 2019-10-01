d742ae694d9dbaa62d7a438260f06c8f0c0fecd0
colethienes <cole.thienes@gmail.com>
2019-10-01 01:23:24 -0400

    .

--- kctl/logger.py
+++ kctl/logger.py
@@ -36,7 +36,7 @@ class KctlLogger:
 
     @staticmethod
     def stack():
-        code = _getframe(2).f_code
+        code = _getframe(3).f_code
         name = code.co_name
         file = code.co_filename[-20:]
         dots = '...' if len(file) == 20 else ''
