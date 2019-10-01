9b9bd24e42388129dc8fce5d77a4bd41ea2678c8
colethienes <cole.thienes@gmail.com>
2019-10-01 02:31:53 -0400

    .

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='koursaros',
     packages=find_packages(),
-    package_data={'': ['*.proto', '*.yaml', '*.gitignore', 'create/*']},
+    package_data={'': ['*.proto', '*.yaml', '*.gitignore'], 'kctl': ['create/*']},
     version='0.0.1',
     license='MIT',
     description='Koursaros is a distributed, cloud-'
