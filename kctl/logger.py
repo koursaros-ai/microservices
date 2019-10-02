a5aa84e584e9ca302f3a7005b20011b04263784b
colethienes <cole.thienes@gmail.com>
2019-10-02 03:14:42 -0400

    .

--- kctl/logger.py
+++ kctl/logger.py
@@ -50,7 +50,7 @@ class KctlLogger:
             func = s2.function
             lineno = s2.lineno
             name = s2.frame.f_globals.get('__name__', '')
-            label = f' {BOLD}[{name}] {func}({lineno}) {GREEN}STDOUT:{RESET}: '
+            label = f' {BOLD}[{name}.{func}({lineno})] {GREEN}STDOUT:{RESET} '
 
         line = cls.timestamp() + label
 
