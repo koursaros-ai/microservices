205e4db9fc1bb3a1c0428601fe7037286e11ceb0
colethienes <cole.thienes@gmail.com>
2019-10-01 23:45:48 -0400

    .

--- examples/pipelines/pig_pipeline/services/backend/__main__.py
+++ examples/pipelines/pig_pipeline/services/backend/__main__.py
@@ -4,12 +4,12 @@ from queue import Queue
 from threading import Thread
 import uuid
 
-pipeline = pig_pipeline(__package__)
+Pipeline = pig_pipeline(__package__)
 app = Flask(__name__)
 sentences = dict()
 
-backend = pipeline.services.backend
-pig = pipeline.services.pig
+backend = Pipeline.Services.backend
+pig = Pipeline.Services.pig
 
 
 @app.route('/')
@@ -25,22 +25,22 @@ def receive():
     sentence_id = str(uuid.uuid4())
     queue = Queue()
     sentences[sentence_id] = queue
-    sentence = backend.stubs.send.Sentence(id=sentence_id, text=text)
-    backend.stubs.send.process(sentence)
+    sentence = backend.Stubs.send.Sentence(id=sentence_id, text=text)
+    backend.Stubs.send.process(sentence)
     return jsonify({
         "status": "success",
         "msg": queue.get()
         })
 
 
-@backend.stubs.send
+@backend.Stubs.send
 def send_sentence(sentence):
     print('SENDING')
     pipeline.services.pig.stubs.piggify(sentence)
     print('SENT')
 
 
-@backend.stubs.receive
+@backend.Stubs.receive
 def receive(piggified):
     global sentences
     sentences[piggified.sentence.id].put(piggified.pig_latin)
