d36f4c425269a0985929de24f17abea334b0572c
colethienes <cole.thienes@gmail.com>
2019-10-01 22:18:47 -0400

    .

--- koursaros/base.py
+++ koursaros/base.py
@@ -93,14 +93,14 @@ class Pipeline:
         self.connections = self._Connections([active_connection_name])
         self.active_connection = getattr(self.connections, active_connection_name)
 
-        if package is not None:
-            active_service_name = package.split('.')[-1]
-            # init services with reference to pipeline
-            self.services = self._Services([active_service_name], self, active_service_name)
+        active_service_name = None if package is None else package.split('.')[-1]
 
-            KctlLogger.init(active_connection_name + '.' + active_service_name)
+        # init services with reference to pipeline
+        self.services = self._Services([active_service_name], self, active_service_name)
+        self.active_service = getattr(self.services, active_service_name)
+        KctlLogger.init(active_connection_name + '.' + active_service_name)
 
-            self.active_service = getattr(self.services, active_service_name)
+
 
 
 
