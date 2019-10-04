688a6a62bd7aaaf77c49cadc7afed57d6be01249
colethienes <cole.thienes@gmail.com>
2019-10-04 12:20:44 -0400

    .

--- kctl/deploy/__init__.py
+++ kctl/deploy/__init__.py
@@ -22,13 +22,13 @@ def deploy(ctx):
 @click.option('-c', '--connection', required=True)
 @click.option('-r', '--rebind', is_flag=True)
 @click.pass_obj
-def pipeline(path_manager, *rmq):
-    rmq_setup(*rmq)
+def pipeline(path_manager, connection, rebind):
+    rmq_setup(connection, rebind)
     pm = path_manager
     from koursaros import pipelines
     pipe = getattr(pipelines, pm.pipe_name)
     services = [cls(service) for service in pipe.Services]
-    subproc_servs(path_manager, services)
+    subproc_servs(path_manager, services, connection)
 
 
 @deploy.command()
@@ -47,11 +47,18 @@ def rmq_setup(connection, rebind):
         bind_rabbitmq(connection)
 
 
-def suproc_servs(pm, services, connection):
+def subproc_servs(pm, services, connection):
+    cmds = []
     for service in services:
-        directory = path_manager.pipe_root + '..'
-        cmd = [sys.executable, '-m', f'{pm.pipe_name}.services.{service}', connection]
-        cmd += sys.argv[1:]
+        cmd = [
+            sys.executable, '-m',
+            f'{pm.pipe_name}.services.{service}',
+            connection, service
+        ]
+        directory = pm.pipe_root + '..'
+        cmds += (directory, cmd)
+
+    subproc(cmds)
 
 
 def subproc(cmds):
@@ -60,29 +67,29 @@ def subproc(cmds):
 
     :param cmds: iterable list of tuples (directory: cmd)
     """
-    processes = []
+    procs = []
 
     try:
         for cmd in cmds:
             print(f'''Running "{BOLD.format(' '.join(cmd))}"...''')
             p = Popen(cmd)
 
-            processes.append((p, cmd))
+            procs.append((p, cmd))
 
-        for p, cmd in processes:
+        for p, cmd in procs:
             p.communicate()
 
     except KeyboardInterrupt:
         pass
 
     finally:
-        for p, service in processes:
+        for p, cmd in procs:
 
             if p.poll() is None:
                 os.kill(p.pid, signal.SIGTERM)
-                print(f'Killing pid {p.pid}: {service}')
+                print(f'Killing pid {p.pid}: {cmd}')
             else:
-                print(f'Process {p.pid}: "{service}" ended...')
+                print(f'Process {p.pid}: "{cmd}" ended...')
 
 
 # else:
