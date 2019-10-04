aa112cd6a88d92c9885ace4b83733b60e5f46227
colethienes <cole.thienes@gmail.com>
2019-10-04 00:15:06 -0400

    .

--- kctl/save/bottler.py
+++ kctl/save/bottler.py
@@ -58,7 +58,7 @@ class ClassBottler:
         """recursive function to indent each subclass nested
         within a ClassCompiler.Class
         """
-        print(self.classes)
+        print([x.__class__.__name__ for x in self.classes])
         raise SystemExit
         indent = '    ' * indents
         indented = [indent + line for line in self.lines]
