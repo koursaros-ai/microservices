34242bac25868363611f359f419dec329242c6ba
Jack Pertschuk <jp954@cornell.edu>
2019-10-04 12:21:19 -0400

    .

--- kctl/train/__init__.py
+++ kctl/train/__init__.py
@@ -1,9 +1,9 @@
 import click
 
 
-@click.group()
+@click.command()
 @click.option('-b', '--base_image', default='koursaros-base')
-@click.pass_context
+@click.pass_obj
 def train(path_manager, base_image):
     """Save current directory's pipeline"""
     print(base_image)
