05c7b20a59ebfe6c922fe92aab8a33ef6aa20935
colethienes <cole.thienes@gmail.com>
2019-10-04 03:11:41 -0400

    .

--- kctl/save/compile.py
+++ kctl/save/compile.py
@@ -144,7 +144,6 @@ class PipelineBottler(ClassBottler):
 
     def reset_imports(self):
         imports = ''
-        import pdb; pdb.set_trace()
         for pipe in self.pm.existing_pipes:
             imports += f'from .{pipe} import {pipe}\n'
 
