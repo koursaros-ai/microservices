869a3048d73818def024d05aab77a4d22484b9d6
Jack Pertschuk <jp954@cornell.edu>
2019-10-05 17:24:15 -0400

    .

--- kctl/train/__init__.py
+++ kctl/train/__init__.py
@@ -1,17 +1,16 @@
 import click
 # from utils.train.roberta_inference import RobertaInference
 import os
-from shared.modeling import model_from_yaml
+from shared.modeling import get_model
 
 @click.command()
 @click.pass_obj
 def train(*args):
     name = args[0]
-    load_model
-    model_from_yaml(name)
+    get_model(name)
 
 @click.command()
 @click.pass_obj
 def eval(*args):
     """Save current directory's pipeline"""
-    main(*args)
+    pass
