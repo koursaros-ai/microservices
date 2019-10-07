ed2c81faad40cdb7c77faf9b64d7c8f9b30555ed
colethienes <cole.thienes@gmail.com>
2019-10-07 12:48:16 -0400

    .

--- kctl/deploy/__init__.py
+++ kctl/deploy/__init__.py
@@ -70,8 +70,8 @@ def service(app_manager, service_name, all=False):
     """Deploy a service"""
     service_yaml_path = app_manager.get_yaml_path(service_name, YamlType.SERVICE)
     service_yaml = Yaml(service_yaml_path)
-    import  pdb; pdb.set_trace()
     base_yaml_path = app_manager.get_yaml_path(service_yaml.base, YamlType.BASE)
+    print(base_yaml_path, service_yaml.base, YamlType.BASE)
 
     if base_yaml_path is None:
         raise FileNotFoundError('Could not find base "%s"' % service_yaml.base)
