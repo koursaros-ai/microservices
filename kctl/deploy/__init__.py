ea41f8cfd84b55987a59d96074e278c91dcc6e27
colethienes <cole.thienes@gmail.com>
2019-10-06 20:05:44 -0400

    .

--- kctl/deploy/__init__.py
+++ kctl/deploy/__init__.py
@@ -1,8 +1,17 @@
 from koursaros import Type, Yaml
 from grpc_tools import protoc
+from typing import List
 from pathlib import Path
+import random
 import click
 import sys
+
+MIN_PORT = 49152
+MAX_PORT = 65536
+
+
+def get_random_ports(num_ports: int) -> List[int]:
+    return random.sample(range(MIN_PORT, MAX_PORT), num_ports)
 
 
 def compile_messages_proto(path):
@@ -16,15 +25,21 @@ def compile_messages_proto(path):
     ))
 
 
+@click.group()
+@click.argument('pipeline_yaml_filename')
+@click.pass_context
+def deploy(ctx):
+    """Deploy a pipeline or service"""
+
+
 @click.command()
-@click.argument('pipeline_yaml_name')
+@click.argument('pipeline_yaml_filename')
 @click.pass_obj
-def deploy(app_manager, pipeline_yaml_filename):
+def pipeline(app_manager, pipeline_yaml_filename):
     """Deploy a pipeline yaml"""
     pipeline_name = Path(pipeline_yaml_filename).stem
     build(app_manager, pipeline_yaml_filename)
     build_yaml = app_manager.search_for_yaml(pipeline_name, Type.BUILD)
-    print(build_yaml)
 
 
 def build(app_manager, pipeline_yaml_filename):
@@ -37,6 +52,16 @@ def build(app_manager, pipeline_yaml_filename):
     pipeline_name = pipeline_yaml_filename.stem
     pipeline_yaml = app_manager.search_for_yaml(pipeline_name, Type.PIPELINE)
 
+    # assign n * 2 ports for the routers
+    number_of_services = len(pipeline_yaml.services)
+    ports = iter(get_random_ports(number_of_services * 2))
+    current_port = next(ports)
+
+    # assign build yaml location
+    build_yaml_dir = app_manager.root.joinpath('build')
+    build_yaml_dir.mkdir(exist_ok=True)
+    build_yaml_path = build_yaml_dir.joinpath(pipeline_yaml_filename)
+
     for service_name in pipeline_yaml.services:
 
         # find each service
@@ -46,17 +71,13 @@ def build(app_manager, pipeline_yaml_filename):
         # find respective base
         base_yaml_path = app_manager.search_for_yaml_path(service_yaml.base, Type.BASE)
         base_yaml = Yaml(base_yaml_path)
-        base_yaml_dir = base_yaml_path.parent
 
         # validate service yaml with base schema
         app_manager.validate_yaml(service_yaml, base_yaml)
 
-        # write build yaml
-        build_yaml_path = app_manager.root.joinpath('build')
-        build_yaml_path.mkdir(exist_ok=True)
-
-        entrypoint = '{}{}{}{}'.format(
-            sys.executable, '-m', pipeline_name, build_yaml_path)
+        port_in = current_port
+        port_out = next(ports)
+        entrypoint = 'kctl deploy service {} {}'.format(service_yaml.base, build_yaml_path)
 
 
         # compile messages for that service
