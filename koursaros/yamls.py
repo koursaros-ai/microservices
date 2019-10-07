f0dddb3a11ee3b12f88ecaba1590a80a27a396d3
colethienes <cole.thienes@gmail.com>
2019-10-07 02:57:55 -0400

    .

--- koursaros/yamls.py
+++ koursaros/yamls.py
@@ -17,21 +17,20 @@ class Yaml(Box):
     :param path: path to .yaml file
     """
     def __init__(self, path):
+        self.__type__ = None
         self.__path__ = path
         self.__text__ = open(path).read()
         yaml = safe_load(self.__text__)
 
-        if 'base' in yaml:
-            self.__type__ = YamlType.BASE
-        elif 'pipeline' in yaml:
-            self.__type__ = YamlType.PIPELINE
-        elif 'service' in yaml:
-            self.__type__ = YamlType.SERVICE
-        else:
+        for yaml_type in YamlType:
+            if yaml_type.name.lower() in yaml:
+                self.__type__ = yaml_type
+
+        if self.__type__ is None:
             raise ValueError('Invalid yaml type for %s' % self.__path__)
 
         super().__init__(yaml)
 
     @property
     def hash(self):
-        return md5(self.__text__).hexdigest()
+        return md5(self.__text__[self.__type__.name.lower()]).hexdigest()
