91d31e5216535264bc04a8181bdb184ecd941fd6
Jack Pertschuk <jp954@cornell.edu>
2019-10-07 15:15:16 -0400

    .

--- koursaros/modeling/__init__.py
+++ koursaros/modeling/__init__.py
@@ -1,7 +1,5 @@
-from yaml import load, dump
-from box import Box
 from koursaros.modeling.models import MODELS
-import hashlib
+from koursaros.yamls import Yaml
 
 def model_filename_resolver(name):
     if name.split('.')[-1] == 'yaml':
@@ -9,14 +7,11 @@ def model_filename_resolver(name):
     return f'./services/{name}.yaml'
 
 def model_from_yaml(filename):
-    with open(filename, 'r') as stream:
-        config = Box(load(stream))
-        md5 = hashlib.md5()
-        md5.update(stream.read().encode())
-        for model_class in MODELS:
-            if config.base in model_class.architectures():
-                model = model_class(config, md5.hexdigest())
-                return model
+    config = Yaml(filename)
+    for model_class in MODELS:
+        if config.base in model_class.architectures():
+            model = model_class(config)
+            return model
 
 def get_model(name):
     filename = model_filename_resolver(name)
