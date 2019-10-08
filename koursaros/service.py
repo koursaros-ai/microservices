f5370c4344b724d953c6e70a40a8dee57c967f6a
colethienes <cole.thienes@gmail.com>
2019-10-07 23:13:32 -0400

    .

--- koursaros/service.py
+++ koursaros/service.py
@@ -185,8 +185,7 @@ class Service:
             body = self._pull()
             msg_tag, body = self._pop_msg_tag(body)
             proto = self._bytes_to_proto(body, self._rcv_proto_cls)
-            self.logger.info(msg_tag)
-            self.logger.info(body)
+            import pdb; pdb.set_trace()
             if msg_tag == self._msg_tag:
                 self._callback(proto)
             else:
