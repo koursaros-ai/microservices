6e842ffd0dfb65285a35c2559363a5687f4569af
Jack Pertschuk <jp954@cornell.edu>
2019-10-08 17:59:13 -0400

    .

--- koursaros/trainer/__main__.py
+++ koursaros/trainer/__main__.py
@@ -2,7 +2,7 @@ import sys
 from koursaros.modeling import model_from_yaml
 
 def train(file):
-    model = model_from_yaml(file, taining=True)
+    model = model_from_yaml(file, training=True)
     model.train()
 
 if __name__ == '__main__':
