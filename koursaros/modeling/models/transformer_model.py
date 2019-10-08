7ffe2784ecec6606193071c1447909252b42d2dc
Jack Pertschuk <jp954@cornell.edu>
2019-10-08 18:40:12 -0400

    .

--- koursaros/modeling/models/transformer_model.py
+++ koursaros/modeling/models/transformer_model.py
@@ -29,7 +29,10 @@ class TransformerModel(Model):
         self.model_config.num_labels = len(self.config.labels)
         self.model = model.from_pretrained(self.checkpoint, config=self.model_config,
                                            cache_dir=self.dir, **kwargs)
-        self.tokenizer = tokenizer.from_pretrained(self.checkpoint, cache_dir=self.dir)
+        try:
+            self.tokenizer = tokenizer.from_pretrained(self.checkpoint, cache_dir=self.dir)
+        except:
+            self.tokenizer = AutoTokenizer.from_pretrained(self.config.arch)
         self.batch_size = 8
         self.max_grad_norm = 1.0
         self.weight_decay = 0.0
