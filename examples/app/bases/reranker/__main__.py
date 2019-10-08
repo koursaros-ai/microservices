78b742cc1b2df9cd9c4ae9f621a0675076822fdb
Jack Pertschuk <jp954@cornell.edu>
2019-10-08 18:21:33 -0400

    .

--- examples/app/bases/reranker/__main__.py
+++ examples/app/bases/reranker/__main__.py
@@ -1,19 +1,19 @@
+from koursaros.utils.misc import batch_fn
 from koursaros.service import Service
 from koursaros.modeling import model_from_config
-from koursaros.utils.misc import batch_fn
 
 service = Service()
 model = model_from_config(service.service_yaml)
 
 @service.stub
-def rerank():
-
-    def score(lines):
-        claims = [claim_with_lines.claim.text] * len(lines)
-        return regression_model.classify(claims, lines)
-    results = []
-    for scores, inputs in batch_fn(BATCH_SIZE, score, claim_with_lines.lines):
-        for score, line in zip(scores, inputs):
-            results.append((score, line))
+def rerank(rerank_query):
+    results = [(model.run(rerank_query.query, line.text), line) for line in rerank_query.lines]
     results.sort(key=lambda x: x[0], reverse=True)
-    print("publishing")
+    return service.Message(
+        query = rerank_query.query,
+        lines=[line[1] for line in results]
+    )
+
+if __name__ == "__main__":
+    service.run()
+
