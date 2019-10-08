58cb4a06e0e1ac6ccbd70a10f84d09d4537d613d
Jack Pertschuk <jp954@cornell.edu>
2019-10-08 19:51:01 -0400

    .

--- examples/app/bases/reranker/__main__.py
+++ examples/app/bases/reranker/__main__.py
@@ -3,13 +3,13 @@ from koursaros.service import Service
 from koursaros.modeling import model_from_config
 
 service = Service()
-model = model_from_config(service.service_yaml)
+model = model_from_config(service.yaml)
 
 @service.stub
 def rerank(rerank_query):
     results = [(model.run(rerank_query.query, line.text), line) for line in rerank_query.lines]
     results.sort(key=lambda x: x[0], reverse=True)
-    return service.Message(
+    return dict(
         query = rerank_query.query,
         lines=[line[1] for line in results]
     )
