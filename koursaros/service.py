1d93e7c6c0b48eb72497807ca29e7f806766638d
colethienes <cole.thienes@gmail.com>
2019-10-07 23:09:20 -0400

    .

--- koursaros/service.py
+++ koursaros/service.py
@@ -185,7 +185,8 @@ class Service:
             body = self._pull()
             msg_tag, body = self._pop_msg_tag(body)
             proto = self._bytes_to_proto(body, self._rcv_proto_cls)
-            self.logger.info(msg_tag, body)
+            self.logger.info(msg_tag)
+            self.logger.info(body)
             if msg_tag == self._msg_tag:
                 self._callback(proto)
             else:
