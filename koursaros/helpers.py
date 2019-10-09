e6043242ae65590d55659781490a158c28c4db5d
colethienes <cole.thienes@gmail.com>
2019-10-09 01:49:00 -0400

    .

--- koursaros/helpers.py
+++ koursaros/helpers.py
@@ -21,7 +21,7 @@ def _parse_msg(body):
     :param body: bytes message
     :return command, msg_id, msg
     """
-    return body[:1], body[1:17], body[17:]
+    return body[:1], body[1:17], body[18:]
 
 
 def _int_to_16byte(integer):
