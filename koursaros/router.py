ed9a613fb2ee0f68b1545574289362dc1ad0a031
colethienes <cole.thienes@gmail.com>
2019-10-08 23:44:37 -0400

    .

--- koursaros/router.py
+++ koursaros/router.py
@@ -39,7 +39,8 @@ class Router:
         _, service_port = get_hash_ports(service, 2)
         service_address = HOST % service_port
         self.service_socket = self.context.socket(zmq.PUSH)
-        self.logger.bold('Connecting PUSH socket to %s' % service_address)
+        self.logger.bold('Connecting PUSH socket to "{}" service on {}'
+                         .format(service, service_address))
         self.service_socket.connect(service_address)
 
     def send_service_command(self, command):
