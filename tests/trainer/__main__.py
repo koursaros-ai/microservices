2bcdcd673e276df8906efe1e6ee76478fb899f01
Jack Pertschuk <jp954@cornell.edu>
2019-10-09 19:56:18 -0400

    .

--- tests/trainer/__main__.py
+++ tests/trainer/__main__.py
@@ -9,15 +9,24 @@ class TrainTest(unittest.TestCase):
     def test_train_classifier(self):
         model = model_from_yaml(os.path.join(dir_path, 'samples/classifier.yaml'))
         model.train(force_build_features=True)
+        assert(os.path.exists(model.ckpt_dir))
 
     def test_train_regression(self):
         model = model_from_yaml(os.path.join(dir_path, 'samples/regression.yaml'))
+        model.train(force_build_features=True)
+        assert (os.path.exists(model.ckpt_dir))
 
     def test_run_classifier(self):
         model = model_from_yaml(os.path.join(dir_path, 'samples/classifier.yaml'))
+        res = model.run(['test'], ['test'])
+        assert(len(res) == 1)
+        assert(type(res[0]) == str)
 
     def test_run_regression(self):
         model = model_from_yaml(os.path.join(dir_path, 'samples/regression.yaml'))
+        res = model.run(['test'], ['test'])
+        assert (len(res) == 1)
+        assert (type(res[0]) == float)
 
 if __name__ == '__main__':
     unittest.main()
