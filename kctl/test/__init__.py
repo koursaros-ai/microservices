c48617e313bc986eb5bf824af72a032e9d563936
colethienes <cole.thienes@gmail.com>
2019-10-10 13:00:23 -0400

    .

--- kctl/test/__init__.py
+++ kctl/test/__init__.py
@@ -29,4 +29,4 @@ def pipeline(ctx, pipeline_name):
 
         logger.bold('POSTING %s on %s' % (translations, url))
         res = requests.post(url, data=translations, headers=headers)
-        logger.bold(json.dumps(res, indent=4))
+        logger.bold(json.dumps(res.content, indent=4))
