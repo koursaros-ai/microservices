49d678e62a21c7c16fe5edec7346a58aa8c324b1
Jack Pertschuk <jp954@cornell.edu>
2019-10-10 15:52:56 -0400

    .

---
+++ koursaros/modeling/migrating.py
@@ -0,0 +1,93 @@
+import sys
+import transformers
+from fairseq.models import roberta
+from fairseq.data.data_utils import collate_tokens
+import time
+import torch.nn.functional as F
+
+# def roberta_to_transformer(path_to_roberta, path_to_yaml):
+#     model = RobertaModel.from_pretrained(path_to_roberta, checkpoint_file='model.pt')
+#     model.eval()
+
+MAX_LENGTH = 256
+PAD = True
+
+def predict_transformers(model, tokenizer):
+    def predict_fn(*args):
+        inputs = time_fn(transformers_encode_batch, tokenizer, *args)
+        outputs = model(*inputs)
+        logits = outputs[0]
+        preds = F.log_softmax(logits, dim=-1)
+        return preds.tolist()
+    return predict_fn
+
+
+def predict_roberta(model):
+    def pred_fn(*args):
+        batch = time_fn(collate_tokens, [model.encode(*arg)[:MAX_LENGTH] for arg in zip(*args)], pad_idx=1)
+        labels = model.predict('mnli', batch).tolist()
+        return labels
+    return pred_fn
+
+def benchmark(pred_fn, n):
+    args = 'All work and no play.', 'Make jack a very dull boy.'
+    for i in range(0, n):
+        assert(type(pred_fn(*args)) == list)
+
+def benchmark_mnli(samples):
+    torch_hub_model = time_fn(roberta.RobertaModel.from_pretrained, 'roberta-large-mnli')
+    transformers_model = time_fn(transformers.RobertaModel.from_pretrained, 'roberta-large-mnli')
+    transformers_tokenizer = time_fn(transformers.RobertaTokenizer.from_pretrained, 'roberta-large-mnli')
+    pred_functions = {
+        'torch_hub' : predict_transformers(transformers_model, transformers_tokenizer),
+        'transformer' : predict_roberta(torch_hub_model)
+    }
+    for framework, pred_fn in pred_functions.items():
+        print(f'Benchmarking {framework} with {samples} samples')
+        time_fn(benchmark, pred_fn, samples)
+
+### HELPERS
+
+def time_fn(fn, *args, **kwargs):
+    start = time.time()
+    res = fn(*args, **kwargs)
+    print(f'Took {time.time() - start} seconds to run {fn.__name__}')
+    return res
+
+
+def transformer_to_features(tokenizer, *args):
+    inputs = tokenizer.encode_plus(
+        *args,
+        add_special_tokens=True,
+        max_length=MAX_LENGTH,
+        truncate_first_sequence=True
+    )
+    input_ids, token_type_ids = inputs["input_ids"][:MAX_LENGTH], \
+                                inputs["token_type_ids"][:MAX_LENGTH]
+
+    attention_mask = [1] * len(input_ids)
+
+    # Zero-pad up to the sequence length.
+    if PAD:
+        padding_length = MAX_LENGTH - len(input_ids)
+        input_ids = ([0] * padding_length) + input_ids
+        attention_mask = ([0] * padding_length) + attention_mask
+        token_type_ids = ([0] * padding_length) + token_type_ids
+
+    return (input_ids, attention_mask, token_type_ids)
+
+
+def transformers_encode_batch(tokenizer, *args):
+    all_input_ids = []
+    all_attention_mask = []
+    all_token_type_ids = []
+    for sample in zip(*args):
+        input_ids, attention_mask, token_type_ids = transformer_to_features(tokenizer, *sample)
+        all_input_ids.append(input_ids)
+        all_attention_mask.append(attention_mask)
+        all_token_type_ids.append(token_type_ids)
+    return all_input_ids, all_attention_mask, all_token_type_ids
+
+
+if __name__ == '__main__':
+    benchmark_mnli(10)
