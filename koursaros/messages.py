65fc985a070cf43c070bc9a120795b9a78d9e113
colethienes <cole.thienes@gmail.com>
2019-10-10 14:19:46 -0400

    .

--- koursaros/messages.py
+++ koursaros/messages.py
@@ -39,7 +39,7 @@ class Messages:
             return proto
 
         elif from_type == MsgType.SEND_PROTO and to_type == MsgType.PROTOBYTES:
-            proto = msg.SerializeToString().encode()
+            proto = msg.SerializeToString()
             return proto
 
         elif from_type == MsgType.JSON and to_type == MsgType.JSONBYTES:
