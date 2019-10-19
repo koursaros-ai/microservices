b6131086c8ec3b4783946d085db8801e192c7895
colethienes <cole.thienes@gmail.com>
2019-10-19 17:00:17 -0400

    .

--- gnes/pipelines/fact-checking/index/flow.py
+++ gnes/pipelines/fact-checking/index/flow.py
@@ -4,17 +4,17 @@ import webbrowser
 flow = (
     Flow(check_version=True)
     .add_preprocessor(name='SentSplitPrep', replicas=2,
-                      yaml_path='../../services/preprocessors/sent-split/json-mode.yml')
+                      yaml_path='../../../services/preprocessors/sent-split/json-mode.yml')
     .add_encoder(name='SiameseBertEncoder', replicas=2,
-                 yaml_path='../../services/encoders/siamese-bert/base.yml')
+                 yaml_path='../../../services/encoders/siamese-bert/base.yml')
     .add_indexer(name='FaissIdx', replicas=2,
-                 yaml_path='../../services/indexers/faiss/base.yml')
+                 yaml_path='../../../services/indexers/faiss/base.yml')
     .add_encoder(name='TextByteEncoder', recv_from='SentSplitPrep', replicas=2,
-                 yaml_path='../../services/encoders/text-byte/max-256.yml')
+                 yaml_path='../../../services/encoders/text-byte/max-256.yml')
     .add_indexer(name='KeywordIdx', replicas=2,
-                 yaml_path='../../services/indexers/keyword/base.yml')
+                 yaml_path='../../../services/indexers/keyword/base.yml')
     .add_indexer(name='LevelDBIdx', recv_from='SentSplitPrep', replicas=2,
-                 yaml_path='../../services/indexers/lvdb/base.yml')
+                 yaml_path='../../../services/indexers/lvdb/base.yml')
     .add_router(name='Reduce', num_part=2, yaml_path='BaseReduceRouter',
                 recv_from=['FaissIdx', 'KeywordIdx', 'LevelDBIdx'])
 )
