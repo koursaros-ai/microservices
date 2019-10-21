25b7eb33f6a79be926620a6c055bb66f6b0ef671
colethienes <cole.thienes@gmail.com>
2019-10-21 17:33:44 -0400

    .

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='koursaros',
     packages=find_packages(),
-    package_data={'koursaros', ['charts/*']},
+    package_data={'koursaros': ['charts/*']},
     version='0.0.1',
     license='MIT',
     description='Koursaros is a distributed, cloud-'
