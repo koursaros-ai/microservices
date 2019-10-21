e9ada53aecaf26e53d8ebcd730d8085cede28a65
colethienes <cole.thienes@gmail.com>
2019-10-21 12:42:50 -0400

    .

--- kctl/decorators.py
+++ kctl/decorators.py
@@ -4,7 +4,7 @@ from ipaddress import ip_address
 
 pipeline_options = decorator_group([
     click.argument('pipeline_name'),
-    click.option('runtime', required=True)
+    click.argument('runtime')
 ])
 
 client_options = decorator_group([
