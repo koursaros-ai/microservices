3afc6f7aad4324904927820ef4434dd22a4b5798
colethienes <cole.thienes@gmail.com>
2019-10-21 18:03:07 -0400

    .

--- kctl/save/__init__.py
+++ kctl/save/__init__.py
@@ -1,6 +1,6 @@
 from ..decorators import *
 import koursaros
-from shutil import copytree
+from shutil import copytree, rmtree
 from pathlib import Path
 
 
@@ -12,22 +12,35 @@ def save():
 @save.command()
 @pipeline_options
 @click.option('-p', '--platform', required=True)
+@click.option('-y', '--yes')
 @click.pass_obj
-def pipeline(app_manager, pipeline_name, runtime, platform):
+def pipeline(app_manager, pipeline_name, runtime, platform, yes):
     """Deploy a pipeline with compose or k8s. """
     flow = app_manager.get_flow('pipelines', pipeline_name, runtime).build()
 
     if platform == 'swarm':
         flow.path.parent.joinpath('docker-compose.yaml').write_text(flow.to_swarm_yaml())
+        app_manager.logger.critical('Saved swarm yaml to %s' % str(flow.path.parent))
 
     elif platform == 'k8s':
-        copytree(
-            str(Path(koursaros.__path__[0]).joinpath('charts', 'gnes')),
-            str(flow.path.parent.joinpath('helm')))
-
-        flow.path.parent.joinpath('gnes', 'values.yaml').write_text(flow.to_helm_yaml())
+        out_path = flow.path.parent.joinpath('helm')
+        if out_path.is_dir():
+            if yes:
+                rmtree(str(out_path))
+            else:
+                while True:
+                    yn = input('Overwrite %s? [y/n]' % str(out_path))
+                    if yn == 'y':
+                        rmtree(str(out_path))
+                        break
+                    elif yn == 'n':
+                        break
+        if not out_path.is_dir():
+            copytree(str(Path(koursaros.__path__[0]).joinpath('charts', 'gnes')), str(out_path))
+            flow.path.parent.joinpath('gnes', 'values.yaml').write_text(flow.to_helm_yaml())
+            app_manager.logger.critical('Saved helm chart to %s' % str(out_path))
 
     else:
         raise NotImplementedError
 
-    app_manager.logger.critical('Saved %s yaml to %s' % (platform, str(flow.path.parent)))
+
