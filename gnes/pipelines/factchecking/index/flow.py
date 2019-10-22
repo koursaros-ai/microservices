f80343b14af42207d9f7502ec3d3aa8acadda7f0
colethienes <cole.thienes@gmail.com>
2019-10-22 10:05:00 -0400

    .

--- gnes/pipelines/factchecking/index/flow.py
+++ gnes/pipelines/factchecking/index/flow.py
@@ -2,14 +2,14 @@ from koursaros.gnes_addons import Flow
 
 flow = (
     Flow(check_version=True)
-    .add_client(name='postgres', yaml_path='clients/postgres/wiki_titles.yml')
+    .add_client(name='postgres', yaml_path='clients/postgres/wikititles.yml')
     .add_preprocessor(name='sentsplit', replicas=2,
-                      yaml_path='services/preprocessors/sent_split/json_mode.yml')
-    .add_encoder(name='textbyte', recv_from='sent_split', replicas=2,
-                 yaml_path='services/encoders/text_byte/max_256.yml')
+                      yaml_path='services/preprocessors/sentsplit/jsonmode.yml')
+    .add_encoder(name='textbyte', recv_from='sentsplit', replicas=2,
+                 yaml_path='services/encoders/textbyte/max256.yml')
     .add_indexer(name='keyword', replicas=2,
                  yaml_path='services/indexers/keyword/base.yml')
-    .add_indexer(name='lvdb', recv_from='sent_split', replicas=2,
+    .add_indexer(name='lvdb', recv_from='sentsplit', replicas=2,
                  yaml_path='services/indexers/lvdb/base.yml')
     .add_router(name='reduce', num_part=2, recv_from=['keyword', 'lvdb'],
                 yaml_path='BaseReduceRouter')
