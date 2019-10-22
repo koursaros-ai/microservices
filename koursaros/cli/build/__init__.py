7e800415e07ba8c10a647742f7a8dd593535aac3
colethienes <cole.thienes@gmail.com>
2019-10-22 18:58:02 -0400

    .

--- koursaros/cli/build/__init__.py
+++ koursaros/cli/build/__init__.py
@@ -20,25 +20,21 @@ def pipeline(app_manager, flow_name, runtime, yes, push, creds):
             raise ValueError('--creds repository must be specified if pushing')
 
         hub_creds = get_creds(creds).dockerhub
-        hub_auth = dict(username=hub_creds.username, password=hub_creds.password)
+        app_manager.subprocess_call('docker login -u %s -p %s' % (
+            hub_creds.username, hub_creds.password), shell=True)
 
     app_manager.subprocess_call('eval $(minikube docker-env)', shell=True)
 
-    docker_client = docker.from_env()
     flow = app_manager.get_flow('flows', flow_name, runtime).build()
     flow.to_helm_yaml()
 
     def docker_build(path, tag):
         app_manager.logger.critical('Building %s from %s...' % (tag, path))
-        image, response = docker_client.images.build(path=path, tag=tag)
-        for res in response:
-            app_manager.logger.info(res)
+        app_manager.subprocess_call('docker build -f %s -t %s' % (tag, path), shell=True)
 
         if push:
             app_manager.logger.critical('Pushing %s...' % tag)
-            response = docker_client.images.push(tag, auth_config=hub_auth)
-            for res in response:
-                app_manager.logger.info(res)
+            app_manager.subprocess_call('docker push %s' % tag, shell=True)
 
     for services in flow.helm_yaml.values():
         for service in services:
@@ -48,6 +44,6 @@ def pipeline(app_manager, flow_name, runtime, yes, push, creds):
 
     if hasattr(flow, '_client_node'):
         client_cls = flow.client_node['name']
-        path = str(app_manager.find('clients', client_cls, 'Dockerfile'))
+        path = str(app_manager.find('clients', client_cls))
         tag = 'gnes-client:%s' % client_cls
         docker_build(path, tag)
