3d76e630a518af1b347324071c49a7353739fcbf
Jack Pertschuk <jp954@cornell.edu>
2019-10-23 18:04:24 -0400

    .

--- koursaros/flows/yc_demo/index/flow.py
+++ koursaros/flows/yc_demo/index/flow.py
@@ -2,7 +2,7 @@ from koursaros.gnes_addons import Flow
 
 flow = (
     Flow(check_version=True)
-    .add_client(name='excel', yaml_path='testrerank.yml') # builds but doesn't deploy
-    .add_indexer(name='keyword', yaml_path='base.yml')
-    .add_router(name='rerank', yaml_path='base.yml')
+    .add_client(name='csv', yaml_path='base.yml') # builds but doesn't deploy
+    .add_indexer(name='keyword', yaml_path='base.yml') # aho corosick
+    .add_router(name='rerank', yaml_path='base.yml') # transformers
 )
