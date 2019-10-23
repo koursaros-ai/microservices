e4a2c21d34e3b1600a1b883722567344ce5d2089
Jack Pertschuk <jp954@cornell.edu>
2019-10-23 19:04:23 -0400

    .

--- koursaros/flows/yc_demo/index/flow.py
+++ koursaros/flows/yc_demo/index/flow.py
@@ -3,6 +3,6 @@ from koursaros.gnes_addons import Flow
 flow = (
     Flow(check_version=True)
     .add_client(name='csv', yaml_path='base.yml') # builds but doesn't deploy
+    .add_encoder(name='textbyte', yaml_path='max256.yml')
     .add_indexer(name='keyword', yaml_path='base.yml') # aho corosick
-    .add_router(name='rerank', yaml_path='base.yml') # transformers
 )
--- koursaros/flows/yc_demo/query/flow.py
+++ koursaros/flows/yc_demo/query/flow.py
@@ -2,7 +2,8 @@ from koursaros.gnes_addons import Flow
 
 flow = (
     Flow(check_version=True)
-    .add_client(name='postgres', yaml_path='testrerank.yml') # builds but doesn't deploy
+    .add_client(name='csv', yaml_path='base.yml')
+    .add_encoder(name='textbyte', yaml_path='max256.yml')
     .add_indexer(name='keyword',yaml_path='base.yml')
     .add_router(name='rerank', yaml_path='base.yml')
 )
