2e0e796a69516070dbdca94b0016692f356427a0
colethienes <cole.thienes@gmail.com>
2019-10-23 17:18:54 -0400

    .

--- koursaros/cli/build/__init__.py
+++ koursaros/cli/build/__init__.py
@@ -12,7 +12,7 @@ def build():
 @pipeline_options
 @click.option('-p', '--push')
 @click.option('-c', '--creds')
-@click.argument('-n', '--no-caches', multiple=True)
+@click.option('-n', '--no-caches', multiple=True)
 def flow(app_manager, flow_name, runtime, push, creds, no_caches):
     """Build images for a pipeline. """
 
