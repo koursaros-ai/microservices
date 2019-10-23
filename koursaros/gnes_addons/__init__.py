c5f42f0a40d1a395407c91edd3f2dbe73f33851c
colethienes <cole.thienes@gmail.com>
2019-10-23 10:51:49 -0400

    .

--- koursaros/gnes_addons/__init__.py
+++ koursaros/gnes_addons/__init__.py
@@ -24,13 +24,28 @@ def dict_merge(dct, merge_dct):
             dct[k] = merge_dct[k]
 
 
-def ignore_invalid_yaml_path(f):
+def add_wrapper(f):
     @functools.wraps(f)
     def wrapped(*args, **kwargs):
-        yaml_path = pathlib.Path(kwargs['yaml_path'])
-        yaml_path.touch()
-        ret = f(*args, **kwargs)
-        yaml_path.unlink()
+        # ignore invalid yaml path
+        path = kwargs['yaml_path']
+        if not path.isidentifier():
+            yaml_path = pathlib.Path(path)
+            yaml_path.touch()
+            ret = f(*args, **kwargs)
+            yaml_path.unlink()
+        else:
+            ret = f(*args, **kwargs)
+
+        # add custom kwargs
+        service = ret._service_nodes[args[1]]
+        service['storage'] = kwargs.get('storage', '500Mi')
+        service['memory'] = kwargs.get('storage', '500Mi')
+        service['cpu'] = kwargs.get('storage', '300m')
+        service['replicas'] = kwargs.get('replicas', 1)
+        import pdb; pdb.set_trace()
+        service['app'] = service['service'].name.lower()
+        # service['model'] =
         return ret
     return wrapped
 
@@ -43,19 +58,19 @@ class Flow(_Flow):
         self.client_node = kwargs
         return self
 
-    @ignore_invalid_yaml_path
+    @add_wrapper
     def add_preprocessor(self, *args, **kwargs):
         return super().add_preprocessor(*args, **kwargs)
 
-    @ignore_invalid_yaml_path
+    @add_wrapper
     def add_encoder(self, *args, **kwargs):
         return super().add_encoder(*args, **kwargs)
 
-    @ignore_invalid_yaml_path
+    @add_wrapper
     def add_indexer(self, *args, **kwargs):
         return super().add_indexer(*args, **kwargs)
 
-    @ignore_invalid_yaml_path
+    @add_wrapper
     def add_router(self, *args, **kwargs):
         return super().add_router(*args, **kwargs)
 
@@ -64,20 +79,13 @@ class Flow(_Flow):
 
         _yaml = YAML()
 
-        extra_parser = argparse.ArgumentParser()
-        extra_parser.add_argument('--storage', default='500Mi')
-        extra_parser.add_argument('--memory', default='500Mi')
-        extra_parser.add_argument('--cpu', default='300m')
-
         services = _yaml.load(self.to_swarm_yaml())['services']
         dict_merge(services, self._service_nodes)
         self.helm_yaml = defaultdict(lambda: [])
 
-        for name, configs in services.items():
+        for name, service in services.items():
             p_args = vars(configs['parsed_args'])
-            extra_args = vars(extra_parser.parse_known_args(configs['unk_args'])[0])
             yaml_path = p_args.get('yaml_path', None)
-            app = configs['service'].name.lower()
 
             build = False
             if isinstance(yaml_path, str):
@@ -90,17 +98,17 @@ class Flow(_Flow):
 
             self.helm_yaml[app] += [dict(
                 name=name,
-                app=app,
+                app=service['app'],
                 model=model,
                 port_in=p_args.get('port_in', None),
                 port_out=p_args.get('port_out', None),
                 ctrl_port=p_args.get('ctrl_port', None),
                 grpc_port=p_args.get('grpc_port', None),
-                command=configs.get('command', None).split(),
-                replicas=configs['deploy'].get('replicas', 1) if 'deploy' in configs else 1,
-                storage=extra_args.get('storage', None),
-                memory=extra_args.get('memory', None),
-                cpu=extra_args.get('cpu', None),
+                command=service.get('command', None).split(),
+                replicas=service['replicas'],
+                storage=service['storage'],
+                memory=service['memory'],
+                cpu=service['cpu'],
                 image='hub-%s:latest-%s' % (app, model) if build else configs['image']
             )]
 
