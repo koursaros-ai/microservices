009ed260ec0d97d6d4fc2ea098dec4503e3716c7
colethienes <cole.thienes@gmail.com>
2019-10-23 15:15:29 -0400

    .

--- koursaros/cli/build/__init__.py
+++ koursaros/cli/build/__init__.py
@@ -54,6 +54,7 @@ def flow(app_manager, flow_name, runtime, push, creds, no_cache):
 
     """save helm chart"""
     out_path = _flow.path.parent.joinpath('helm')
+    rmtree(str(out_path), ignore_errors=True)
     copytree(str(app_manager.find('chart', pkg=True)), str(out_path))
     _flow.path.parent.joinpath('helm/values.yaml').write_text(helm_yaml)
     app_manager.logger.critical('Saved helm chart to %s' % str(out_path))
