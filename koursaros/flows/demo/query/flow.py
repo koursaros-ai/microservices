a964e9b96d0afad30fb78933b862445750174f85
Jack Pertschuk <jp954@cornell.edu>
2019-10-23 17:04:39 -0400

    .

--- koursaros/flows/demo/query/flow.py
+++ koursaros/flows/demo/query/flow.py
@@ -2,7 +2,7 @@ from koursaros.gnes_addons import Flow
 
 flow = (
     Flow(check_version=True)
-    .add_client(name='postgres', yaml_path='client/postgres/testrerank.yml')
-    .add_indexer(name='keyword',yaml_path='indexer/keyword/base.yml')
-    .add_router(name='rerank', yaml_path='router/rerank/base.yml')
+    .add_client(name='postgres', yaml_path='testrerank.yml') # builds but doesn't deploy
+    .add_indexer(name='keyword',yaml_path='base.yml')
+    .add_router(name='rerank', yaml_path='base.yml')
 )
