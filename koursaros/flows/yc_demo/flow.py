7d54bf535e40086bc7e1a94d3cb53be8b5f45751
colethienes <cole.thienes@gmail.com>
2019-10-24 11:13:49 -0400

    .

--- koursaros/flows/yc_demo/flow.py
+++ koursaros/flows/yc_demo/flow.py
@@ -5,7 +5,7 @@ flow = (
     .add_client() # builds but doesn't deploy
     .add_frontend(copy_flow=False)
     .add_router(yaml_path='BaseUnaryRouter')
-    .add_encoder(name='textbyte', yaml_path='max256.yml', recv_from=['pubber'])
+    .add_encoder(name='textbyte', yaml_path='max256.yml')
     .add_indexer(name='keyword', yaml_path='base.yml') # aho corosick
     .add_router(yaml_path='BaseUnaryRouter', recv_from=['Router0'])
     .add_router(name='rerank', yaml_path='base.yml', recv_from=['keyword', 'reroute'])
