41d04c36b46226a8176e4565b53be0a30e445fff
colethienes <cole.thienes@gmail.com>
2019-10-24 10:59:45 -0400

    .

--- koursaros/flows/yc_demo/flow.py
+++ koursaros/flows/yc_demo/flow.py
@@ -2,7 +2,7 @@ from koursaros.gnes_addons import Flow
 
 flow = (
     Flow(check_version=True)
-    .add_client(yaml_path='base.yml') # builds but doesn't deploy
+    .add_client() # builds but doesn't deploy
     .add_router(yaml_path='BaseUnaryRouter')
     .add_encoder(name='textbyte', yaml_path='max256.yml', recv_from=['pubber'])
     .add_indexer(name='keyword', yaml_path='base.yml') # aho corosick
