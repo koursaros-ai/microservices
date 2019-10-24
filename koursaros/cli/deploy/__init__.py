b9b89261c9ebd6ed96b49c6ac62d7acb6255f6f0
colethienes <cole.thienes@gmail.com>
2019-10-24 14:51:37 -0400

    .

--- koursaros/cli/deploy/__init__.py
+++ koursaros/cli/deploy/__init__.py
@@ -59,7 +59,7 @@ def k8s(app_manager, flow_name, dryrun):
 @click.pass_obj
 def client(app_manager, client_name):
     """Deploy a client. """
-    path = app_manager.find_model('client', client_name)#.joinpath('client.py')
+    path = app_manager.find_model('client', client_name).joinpath('clienty.py')
     if not path.exists():
         raise FileNotFoundError('Could not find %s' % path)
     importlib.util.spec_from_file_location(client_name, path)
