ae91a35725a99fe4974ed251e09a994ed5ebbb11
colethienes <cole.thienes@gmail.com>
2019-10-24 10:57:05 -0400

    .

--- koursaros/flows/yc_demo/flow.py
+++ koursaros/flows/yc_demo/flow.py
@@ -2,10 +2,10 @@ from koursaros.gnes_addons import Flow
 
 flow = (
     Flow(check_version=True)
-    .add_client(name='csv', yaml_path='base.yml') # builds but doesn't deploy
-    .add_router(name='pubber', yaml_path='base.yml')
+    .add_client(yaml_path='base.yml') # builds but doesn't deploy
+    .add_router(yaml_path='BaseUnaryRouter')
     .add_encoder(name='textbyte', yaml_path='max256.yml', recv_from=['pubber'])
     .add_indexer(name='keyword', yaml_path='base.yml') # aho corosick
-    .add_router(name='reroute', yaml_path='base.yml', recv_from=['pubber'])
+    .add_router(yaml_path='BaseUnaryRouter', recv_from=['pubber'])
     .add_router(name='rerank', yaml_path='base.yml', recv_from=['keyword', 'reroute'])
 )
