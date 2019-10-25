4a10673bea5cddac8a2cd467d4420eb89e507c88
Jack Pertschuk <jp954@cornell.edu>
2019-10-25 12:28:24 -0400

    .

--- koursaros/hub/router/rerank/rerank.py
+++ koursaros/hub/router/rerank/rerank.py
@@ -66,6 +66,7 @@ class RerankRouter(BaseReduceRouter):
         if len(inputs) == 0:
             print("Warning: empty input set, ignoring.")
             super().apply(msg, accum_msgs)
+            return
 
         max_len = max(len(t['input_ids']) for t in inputs)
         input_ids = [t['input_ids'] + [0] * (max_len - len(t['input_ids'])) for t in inputs]
