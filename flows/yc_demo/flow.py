57f6a78d3c49bc2315b638227b41d938d03938d3
colethienes <cole.thienes@gmail.com>
2019-10-25 19:09:12 -0400

    Merge branch 'master' of https://github.com/koursaros-ai/koursaros

--- flows/yc_demo/flow.py
+++ flows/yc_demo/flow.py
@@ -7,7 +7,7 @@ flow = (
     .add_frontend(copy_flow=False)
     .add_router(yaml_path='BaseRouter')
     .add_preprocessor(name='unary', yaml_path='text.yml', doc_type=1)
-    .add_encoder(name='textbyte', yaml_path='max256.yml')
+    .add_encoder(name='textbyte', yaml_path='max1024.yml')
     .add_indexer(name='keyword', yaml_path='base.yml') # aho corosick
     .add_router(yaml_path='BaseRouter', recv_from=['Router0'])
     .add_router(name='rerank', yaml_path='base.yml', recv_from=['keyword', 'Router1'])
