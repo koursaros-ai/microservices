1eb05ef80ee304b6712434957ab74aa2e3372d87
Jack Pertschuk <jp954@cornell.edu>
2019-10-27 13:18:57 -0700

    .

--- koursaros/hub/tests/test_textbyte_encoder.py
+++ koursaros/hub/tests/test_textbyte_encoder.py
@@ -1,7 +1,15 @@
 import unittest
-from koursaros.hub.encoder.textbyte import
+from koursaros.hub.encoder.textbyte.textbyte import TextByteEncoder
 
 class TestTextByte(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.model =
+        self.model = TextByteEncoder(64)
+
+    def test_textbyte(self):
+        vectors = self.model.encode([
+            "This is a really long sentence it's really long ",
+            "This is a shorter sentence",
+            "This"])
+        for vec in vectors:
+            print(len(vec))
